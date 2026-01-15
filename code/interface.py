0b7a4bba8b05ef9effbfb4c1e626fb9dd04a0dfd
lefitous <47494452+lefitous@users.noreply.github.com>
2026-01-15 08:38:37 +0100

    changed classes fix

--- code/interface.py
+++ code/interface.py
@@ -9,7 +9,7 @@ class Interface():
         self.name = name
         self.address = ""
         self.neighbors_address = []
-        self.protocol_list = []
+        self.protocol = ""
 
     def __str__(self):
         print(f"interface name : {self.name} \ninterface address : {self.address} \ninterface protocol : {self.protocol}")
