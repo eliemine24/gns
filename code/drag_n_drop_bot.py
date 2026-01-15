b8b51772a21b3d88576693af9ddb17337e8d2249
eliemine24 <elie.gautier24@gmail.com>
2026-01-15 09:37:51 +0100

    place_file

--- code/drag_n_drop_bot.py
+++ code/drag_n_drop_bot.py
@@ -23,14 +23,21 @@ INTENT = json_to_dict("intent_file.json")       # fichier d'intention
 
 
 # Récupérer les noms imbuvables des dossier de config
+def find_repository_names(path, ):
 
-def find_repository_names(path):
+    # lire le fichier "project_name"+".gns3"
+    # en faire un dictionnaire (json_ton_dict() ça doit fonctionner)
+    # récupérer les noms de fichier correspondans aux noms des routeurs ("R1", "R2", etc)
+    # renvoyer un un dict ou une liste de tuples {"router_name" : "repository_name"; etc} / [("router_name", "repository_name"), etc]
+
     pass
 
-# Déplacer les fichier au bon endroit jsp comment
-def place_files(path):
+# Déplacer un fichier fichier file depuis l'emplacement du script jusqu'à l'emplacement final
+def place_file(file, project_path, path_to_file):
     pass
+
+
 
 
 # TEST omg ça marche (créer un "testfile.py dans le dossier local")
-shutil.move(PATH+"/testfile.py", HPATH+"/")
+#shutil.move(PATH+"/testfile.py", HPATH+"/")
