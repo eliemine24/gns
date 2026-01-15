50ee93639ae9de1410f34b62919c121bc649610a
eliemine24 <elie.gautier24@gmail.com>
2026-01-15 10:39:55 +0100

    dragndrop

--- code/generate_classes.py
+++ code/generate_classes.py
@@ -70,19 +70,3 @@ def generate_interface(interface_name, interface_info, as_obj):
     else:
         interface.protocol_list.append(interface_info.get("PROTOCOL",""))
     return interface
-
-
-# =======================
-# === TESTS FONCTIONS ===
-# =======================
-# (ça fonctionne)
-
-local_path = find_local_path()
-print(f"local path : {local_path}")
-
-router_list = generate_network_classes(local_path + "/intent_file.json")
-
-for r in router_list:
-    print(f"{r.name} (AS {r.AS_name}) | {r.ID} | {r.nb_int}")
-    for i in r.liste_int:
-        print(f"  {i.name} {i.address} {i.protocol_list} neighbors: {i.neighbors_address}")
